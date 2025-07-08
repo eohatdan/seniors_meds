558a405c86bd189f6e43e12cbf41ed83cf1fe208
Danny Clarke <daninreno@gmail.com>
2025-07-08 09:31:28 -0700

    Update server.py

--- server.py
+++ server.py
@@ -146,4 +146,9 @@ def is_float(value):
 
 # Run for local dev (optional)
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # fallback for local
+    app.run(host="0.0.0.0", port=port)
+
