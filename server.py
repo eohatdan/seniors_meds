04f20e148da42a14b92440cc95405eed46b890ed
Danny Clarke <daninreno@gmail.com>
2025-07-13 20:29:24 -0700

    Update server.py

--- server.py
+++ server.py
@@ -191,6 +191,7 @@ def admin_reset_password():
 
 
 # ========== Render Entry Point ==========
+  
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
